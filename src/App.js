b993e51a90224f3c4fc366c8bd0277f1003ee6fe
timtiph <tiphany.moine@outlook.com>
2022-11-03 13:54:03 +0100

    mise en place dossier/fichier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 // import Item from './Components/Item/item';
 
 
 function App() {
 
 
-  const [toggle, setToggle] = useState(true);
-
-  const changeState = () => {
-    setToggle(!toggle);
-  };
-
-
-
-
-  return (
-    <div className="App">
-       <div className={toggle ? "box animated" : "box"}></div>
-      <button onClick={changeState}>Change state</button>
-    </div>
-  );
-
+return (
+  <div className="App">
+    <h1>Bonjour</h1>
+  </div>
+);
 
 
 
