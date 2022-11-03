704c3f2168f7e13bd2351a5d5132db11d62fc56f
timtiph <tiphany.moine@outlook.com>
2022-11-03 14:59:16 +0100

    formulaire

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-// import { useState } from 'react';
-// import Item from './Components/Item/item';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Form from './Components/Form'
 
 
 function App() {
 
 
-return (
-  <div className="App">
-    <h1>Bonjour</h1>
-    <button className="btn btn-danger">Boutton BOOTSTRAP 5 couleur danger</button>
-  </div>
-);
+  return (
+    <div className="App">
+      <h1 className="text-center mt-3">Todo-List</h1>
+      <Form>
 
-
-
+      </Form>
+    </div>
+  );
 }
 
 export default App;
