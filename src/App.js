16a2930a6994e1a76e62d9359505a223d755671c
timtiph <tiphany.moine@outlook.com>
2022-11-03 10:33:57 +0100

    création item.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Item from './item'
+
+
 function App() {
   return (
     <div className="App">
-      <h1>HELLO WORLD</h1>
+      <h1>HELLO APP REACT</h1>
+      <Item></Item>
+
     </div>
   );
 }
