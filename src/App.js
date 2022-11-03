45442f274864aad8ee561a61a895b2ca055791ad
timtiph <tiphany.moine@outlook.com>
2022-11-03 13:01:59 +0100

    if else affichage de contenu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-// import { useState } from 'react';
-import Item from './Components/Item/item';
+import { useState } from 'react';
+// import Item from './Components/Item/item';
 
 
 function App() {
 
+
+  const [toggle, setToggle] = useState(true);
+
+  const changeState = () => {
+    setToggle(!toggle);
+  };
+
+
+
+  // let toggleContenu;
+
+  // if (toggle) {
+  //   toggleContenu = <h1>Le state est True</h1>;
+  // } else {
+  //   toggleContenu = <h1>Le state est False</h1>;
+  // }
+
+
+
   return (
     <div className="App">
-     <h1>Hello depuis App</h1>
-      <Item></Item>
+      {toggle ? <h1>Le state est TRUE</h1> : <h1>Le state est FALSE</h1> } 
+      <button onClick={changeState}>Change state</button>
     </div>
   );
+
+
+
+
 }
 
 export default App;
