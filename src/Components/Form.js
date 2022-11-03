7f80e55c11d47e93b853a107a9a5728972005500
timtiph <tiphany.moine@outlook.com>
2022-11-03 15:20:10 +0100

    ajout de tache à la ToDo list

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -9,6 +9,7 @@ export default function Form() {
         { txt: "Sport", id: uuidv4() },
         { txt: "Coder avec réact", id: uuidv4() },
     ])
+    const [stateInput, setStateInput] = useState();
 
     const deleteElement = id => {
         // console.log(id);
@@ -18,16 +19,39 @@ export default function Form() {
         setDataArr(filteredState)
     }
 
+    const addTodo = e => {
+        e.preventDefault(); // empeche la mise à jour à chaque click sur envoyer
+        const newArr = [...dataArr] // création copie du state (du tableau) pour travailler dessus
+
+        const newTodo = {}; // création nouvel obj
+        newTodo.txt = stateInput; // ajoute une propriété txt
+        newTodo.id = uuidv4(); // on ajoute un id
+
+        newArr.push(newTodo); // push le nouvel obj dans la copie du tableau 
+        setDataArr(newArr);  // on change le state avec la copie du tableau 
+
+        setStateInput('') // remet l'input à 0
+    }
+
+    const linkedInput = e => {
+        // console.log(e);
+        setStateInput(e);
+    }
 
     return (
 
         <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
 
-            <form className="mb-3">
-                <label htmlFor="todo" className="form-label mt-3">Chose à faire</label>
-                <input type="text" className="form-control" id="todo" />
+            <form onSubmit={e => addTodo(e)} className="mb-3">
+                <label htmlFor="todo" className="form-label mt-3">Chose à faire : </label>
+                <input
+                    value={stateInput}
+                    onChange={e => linkedInput(e.target.value)}
+                    type="text"
+                    className="form-control"
+                    id="todo" />
 
-                <button className="mt-2 btn btn-primary d-block">Envoyez</button>
+                <button className="mt-2 btn btn-primary d-block">Ajouter à la liste</button>
             </form>
 
             <h2>Liste des choses à faire : </h2>
