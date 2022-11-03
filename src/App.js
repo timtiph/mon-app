1c194e7788afcfc9dc597d696d489e1aa5f20924
timtiph <tiphany.moine@outlook.com>
2022-11-03 14:02:51 +0100

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import { useState } from 'react';
 // import Item from './Components/Item/item';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 return (
   <div className="App">
     <h1>Bonjour</h1>
+    <button className="btn btn-danger">Boutton BOOTSTRAP 5 couleur danger</button>
   </div>
 );
 
